8dff1a15aac840d74015e677d42e9f2fab5ad86f
Tayomide <tibrahim@hawk.iit.edu>
2022-08-23 19:25:43 -0500

    Change default syntax
    AppContainer was generic and overflow: hidden was not necessary for this project

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
 const App = () => {
   return(
-    <AppContainer>
+    <Container>
       <Router>
         <Routes>
           <Route exact path="/" element={ <div>Hey Yinka! It's tomiwa</div> } />
         </Routes>
       </Router>
-    </AppContainer>
+    </Container>
   )
 }
 
